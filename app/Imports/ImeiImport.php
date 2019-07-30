b3d5b3fd76976b04df9e4b2ccda24a08934de5d4
Eduardo Ortega <ortegon000@hotmail.com>
2019-07-30 15:22:15 -0500

    x

--- app/Imports/ImeiImport.php
+++ app/Imports/ImeiImport.php
@@ -5,27 +5,38 @@ namespace App\Imports;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\RegistersEventListeners;
 use Maatwebsite\Excel\Concerns\ToArray;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Events\BeforeImport;
+use PhpOffice\PhpSpreadsheet\Settings;
 
 class ImeiImport implements ToArray, WithChunkReading, WithHeadingRow, ShouldQueue
 {
-    use Importable;
+    use Importable, RegistersEventListeners;
 
     public $filename;
     public $timeOut = 20000;
     public $tries = 3;
 
+    public $sum = 0;
+
     public function __construct($fileName)
     {
+        set_time_limit ( 3200 );
         $this->filename = $fileName;
+    }
+
+    public static function beforeImport(BeforeImport $event)
+    {
+        $options = LIBXML_COMPACT | LIBXML_PARSEHUGE;
+
+        Settings::setLibXmlLoaderOptions($options);
     }
 
     public function array(array $rows)
     {
-        set_time_limit ( 3200 );
-
         foreach ($rows as $row) {
             $code2 = strpos($row['codigo2'], ',') ? explode(',', $row['codigo2'])[0] : $row['codigo2'];
             DB::table('tblcodigos')->insert([
@@ -35,11 +46,13 @@ class ImeiImport implements ToArray, WithChunkReading, WithHeadingRow, ShouldQue
                 'codigo1' => $row['codigo'] ?? '',
                 'codigo2' => $code2 ?? '',
             ]);
+
+            $this->sum++;
         }
     }
 
     public function chunkSize(): int
     {
-        return 2500;
+        return 10000;
     }
 }
