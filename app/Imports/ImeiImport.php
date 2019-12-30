83297c7875608b6bfc4dd186d0bd34723e666a27
Eduardo Ortega <ortegon000@hootmail.com>
2019-12-30 11:11:07 -0600

    x

--- app/Imports/ImeiImport.php
+++ app/Imports/ImeiImport.php
@@ -31,15 +31,24 @@ class ImeiImport implements ToArray, WithChunkReading, WithHeadingRow, ShouldQue
     public function array(array $rows)
     {
         foreach ($rows as $row) {
-            $code2 = strpos($row['codigo2'], ',') ? explode(',', $row['codigo2'])[0] : $row['codigo2'];
+            if (
+                isset($row['imei']) &&
+                isset($row['marca']) &&
+                isset($row['modelo'])
+            ) {
+                $code2 = strpos($row['codigo2'], ',') ? explode(',', $row['codigo2'])[0] : $row['codigo2'];
 
-            DB::table('tblcodigos')->insert([
-                'imei'    => $row['imei'] ?? '',
-                'marca'   => $row['marca'] ?? '',
-                'modelo'  => $row['modelo'] ?? '',
-                'codigo1' => $row['codigo'] ?? '',
-                'codigo2' => $code2 ?? '',
-            ]);
+                DB::table('tblcodigos')->insert([
+                    'imei'    => $row['imei'] ?? '',
+                    'marca'   => $row['marca'] ?? '',
+                    'modelo'  => $row['modelo'] ?? '',
+                    'codigo1' => $row['codigo'] ?? '',
+                    'codigo2' => $code2 ?? '',
+                ]);
+            }
+            else {
+                (new ConsoleOutput)->writeln("El archivo a subir no esta basado en la plantilla de importacion");
+            }
 
             $this->sum++;
         }
