be9675583d429fdaa5d7785da602c50e6cf19063
Eduardo Ortega <ortegon000@hootmail.com>
2019-12-30 10:38:38 -0600

    move home route code to controller

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@
 */
 
 Route::get('/', 'ImeiController@home')->name('home');
-
 Route::post('upload-excel', 'ImeiController@uploadExcel')->name('upload_excel');
 Route::get('download-excel/template', 'ImeiController@downloadExcelTemplate')->name('download_excel.template');
 Route::get('duplicate-excel', 'ImeiController@deleteDuplicates')->name('delete_duplicates');
